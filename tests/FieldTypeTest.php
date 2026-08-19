b19f5bf3eb59475893d2397b5cdedc7bf7bec109
Imants Horsts <imants.horsts@inbox.lv>
2026-08-19 23:13:06 +0300

    test

--- tests/FieldTypeTest.php
+++ tests/FieldTypeTest.php
@@ -22,7 +22,7 @@ class TestModel extends Model
         parent::init();
 
         // all field types
-        $this->getField($this->getIdField())->type = 'integer';
+        $this->getIdField()->type = 'integer';
         $this->addField('f_string', ['type' => 'string']);
         $this->addField('f_text', ['type' => 'text']);
         $this->addField('f_boolean', ['type' => 'boolean']);
