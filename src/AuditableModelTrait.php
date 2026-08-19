80186b1eef1de512170c63adcbae2e3e7a217b5b
Imants Horsts <imants.horsts@inbox.lv>
2026-08-19 22:45:50 +0300

    typo

--- src/AuditableModelTrait.php
+++ src/AuditableModelTrait.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Atk4\Audit;
 
-class AuditableModelTrait;
+class AuditableModelTrait
 {
     /** @var bool Should we audit this model */
     public $no_audit = true;
